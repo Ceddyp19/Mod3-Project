908fdc86d4b84c7ca06af3d3ad292d10429104f5
Ceddyp19 <ceddyp117@gmail.com>
2020-09-08 14:58:14 -0500

    created custom serializer for scores

--- Back-end/app/controllers/scores_controller.rb
+++ Back-end/app/controllers/scores_controller.rb
@@ -1,14 +1,28 @@
 class ScoresController < ApplicationController
-    def index 
-        scores = Score.all 
-        render json: ScoreSerializer.new(scores)
-    end
+    # def index 
+    #     scores = Score.all 
+    #     options = {
+    #         include: [:player, :game]
+    #     }
+    #     render json: ScoreSerializer.new(scores, options)
+    # end
 
-    def show 
-        score = Score.find_by(params[:id])
-        options = {
-            include: [:player, :game]
-        }
-        render json: ScoreSerializer.new(score, options)
-    end
+    # def show 
+    #     score = Score.find_by(params[:id])
+    #     options = {
+    #         include: [:player, :game]
+    #     }
+    #     render json: ScoreSerializer.new(score, options)
+    # end
+
+      def index
+        scores = Score.all
+        render json: ScoreSerializer.new(scores).to_serialized_json
+      end
+     
+      def show
+        score = Score.find_by(id: params[:id])
+        render json: ScoreSerializer.new(score).to_serialized_json
+      end
+    
 end
--- Back-end/app/serializers/score_serializer.rb
+++ Back-end/app/serializers/score_serializer.rb
@@ -1,6 +1,28 @@
+# class ScoreSerializer
+#   include FastJsonapi::ObjectSerializer
+#   attributes :score
+#   belongs_to :game
+#   belongs_to :player 
+# end
+
 class ScoreSerializer
-  include FastJsonapi::ObjectSerializer
-  attributes :score
-  belongs_to :game
-  belongs_to :player 
+  def initialize(score_object)
+    @score = score_object
+  end
+
+  def to_serialized_json
+    options = {
+      include: {
+        player: {
+          only: [:name, :id]
+        },
+        game: {
+          only: [:id, :created_at]
+        }
+      },
+      except: [:updated_at],
+    }
+    @score.to_json(options)
+  end
+
 end
